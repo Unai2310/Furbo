4761603af32f3ec5a281977a553888495241b025
Unai2310 <unaiyanton@gmail.com>
2024-12-26 18:56:31 +0100

    Primera Subida

---
+++ Furbo/FrmJugador.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Furbo
+{
+    public partial class FrmJugador : Form
+    {
+        Stats stats;
+        String nombre;
+        Dictionary<string, string> imagenes;
+        public FrmJugador(String nombre, Stats stats)
+        {
+            InitializeComponent();
+            imagenes = new Dictionary<string, string>()
+            { 
+                { "Dani", "" },
+                { "JuanPa", "https://files.catbox.moe/wt8f3n.jpeg" },
+                { "Rurru", "https://files.catbox.moe/rdqgvi.jpeg" },
+                { "Juanjo", "https://files.catbox.moe/t29c1s.jpeg" },
+                { "Oscar", "https://files.catbox.moe/l7ipe7.jpeg" },
+                { "Puvero", "https://files.catbox.moe/u6tzv6.jpeg" },
+                { "Raul", "https://files.catbox.moe/vdrlz2.jpeg" },
+                { "Javixu", "https://files.catbox.moe/gu0b1m.jpeg" },
+                { "Pito", "https://files.catbox.moe/rnl8ql.jpeg" },
+                { "Unai", "https://files.catbox.moe/foovf2.jpeg" },
+                { "Davisote", "https://files.catbox.moe/sqlars.jpeg" },
+                { "Davisito", "https://files.catbox.moe/etbhuo.jpeg" },
+                { "Felipe", "https://files.catbox.moe/akw8vk.jpeg" },
+                { "Almendra", "https://files.catbox.moe/ysjsn0.jpeg" },
+                { "Pol", "https://files.catbox.moe/1dopg4.jpeg" },
+                { "Invitados", "https://files.catbox.moe/a7hy8q.jpeg" }
+            };
+            this.stats = stats;
+            this.Text = nombre;
+            this.nombre = nombre;
+        }
+
+        private void FrmJugador_Load(object sender, EventArgs e)
+        {
+            lblNombreData.Text = this.nombre;
+            lblPichichiData.Text = this.stats.pichichis.ToString();
+            lblPromedioData.Text = this.stats.promedio.ToString("F2");
+            lblPuskasData.Text = this.stats.puskas.ToString();
+            lblTotalesData.Text = this.stats.totales.ToString();
+            lblWrData.Text = this.stats.wr.ToString("F2") + "%";
+            LblJugadosData.Text = this.stats.jugados.ToString();
+            foreach (KeyValuePair<String, String> entry in imagenes)
+            {
+                if (entry.Key.Equals(this.nombre))
+                {
+                    pcbFoto.Load(entry.Value);
+                }
+            }
+        }
+    }
+}
---
+++ Furbo/Jornada.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furbo
+{
+    internal class Jornada
+    {
+        public String nombre;
+        public List<Jugador> equipoGanador;
+        public List<Jugador> equipoPerdedor;
+        public List<Jugador> bajas;
+        public List<Jugador> todos;
+        public int resultadoGanador;
+        public int resultadoPerdedor;
+        public String max;
+        public String total;
+        public String pichichi;
+        public String puskas;
+        public String fecha;
+
+        public Jornada(Dictionary<string, string> jornada, string nombre, Dictionary<String, Stats> stats)
+        { 
+            List<String> campos = new List<String>();
+            campos.Add("Max");
+            campos.Add("Total");
+            campos.Add("Pichichi");
+            campos.Add("Puskas");
+            campos.Add("fecha");
+            this.nombre = nombre;
+            equipoGanador = new List<Jugador>();
+            equipoPerdedor = new List<Jugador>();
+            bajas = new List<Jugador>();
+            todos = new List<Jugador>();
+            this.max = jornada.ElementAt(16).Value;
+            this.total = jornada.ElementAt(17).Value;
+            this.pichichi = jornada.ElementAt(18).Value;
+            this.puskas = jornada.ElementAt(19).Value;
+            this.fecha = jornada.ElementAt(20).Value;
+            foreach (KeyValuePair<string, string> entry in jornada)
+            {
+                if (!campos.Contains(entry.Key))
+                {
+                    Jugador jugador = new Jugador(entry.Key, entry.Value);
+                    clasificarJugadores(jugador, stats);
+                    todos.Add(jugador);
+                }
+            }
+            this.nombre = nombre;
+        }
+
+        private void clasificarJugadores(Jugador jugador, Dictionary<String, Stats> stats)
+        {
+            if (jugador.resultado.Equals("0"))
+            {
+                this.equipoGanador.Add(jugador);
+                this.resultadoGanador += Int32.Parse(jugador.goles);
+                foreach(KeyValuePair<string, Stats> entry in stats)
+                {
+                    if (jugador.nombre.Equals(entry.Key))
+                    {
+                        entry.Value.jugados++;
+                        entry.Value.totales += Int32.Parse(jugador.goles);
+                        entry.Value.victorias++;
+                        if (jugador.goles.Equals(this.max))
+                        {
+                            entry.Value.pichichis++;
+                        }
+                        if (jugador.nombre.Equals(this.puskas))
+                        {
+                            entry.Value.puskas++;
+                        }
+                    }
+                }
+            } 
+            else if (jugador.resultado.Equals("1"))
+            {
+                this.equipoPerdedor.Add(jugador);
+                this.resultadoPerdedor += Int32.Parse(jugador.goles);
+                foreach (KeyValuePair<string, Stats> entry in stats)
+                {
+                    if (jugador.nombre.Equals(entry.Key))
+                    {
+                        entry.Value.jugados++;
+                        entry.Value.totales += Int32.Parse(jugador.goles);
+                        if (jugador.goles.Equals(this.max))
+                        {
+                            entry.Value.pichichis++;
+                        }
+                        if (jugador.nombre.Equals(this.puskas))
+                        {
+                            entry.Value.puskas++;
+                        }
+                    }
+                }
+            } 
+            else
+            {
+                this.bajas.Add(jugador);
+            }
+        }
+    }
+}
